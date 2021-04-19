85bd22accd9b01f911a44e8a20e95978b68a40b2
Dalton Xue <dalousha@gmail.com>
2021-04-19 19:00:40 -0400

    Added some styles, implemented load more answers/hide answers functionality to the questions

--- client/src/components/Questions/components/Answer.jsx
+++ client/src/components/Questions/components/Answer.jsx
@@ -12,6 +12,7 @@ class Answer extends React.Component {
     }
 
     this.getAnswers = this.getAnswers.bind(this);
+    this.loadMoreAnswers = this.loadMoreAnswers.bind(this);
   }
 
   componentDidMount() {
@@ -31,6 +32,24 @@ class Answer extends React.Component {
     })
   }
 
+  loadMoreAnswers() {
+    var elem = document.getElementById('loadMoreAnswersLink')
+
+
+    if (elem.innerHTML === 'Load More Answers') {
+      elem.innerHTML = 'Hide Answers';
+      this.state.answers.slice(2, this.state.answers.length).forEach(answer => {
+        document.getElementById('second-default-answer').innerHTML += `<div class='extraAnswers'>A: ${answer.body}<br /> <span>by ${answer.answerer_name}</span> <span>Helpful? </span> <u>Yes</u> (${answer.helpfulness}) <u>Report</u> </div>`
+      });
+    } else {
+      elem.innerHTML = 'Load More Answers';
+      var toDelete = document.getElementsByClassName('extraAnswers')
+      while (toDelete.length > 0) {
+        toDelete[0].remove();
+      }
+    }
+
+  }
 
 
   render() {
@@ -42,11 +61,11 @@ class Answer extends React.Component {
       </div>
     } else {
     return (
-      <div>
-        {(this.state.answers[0]) ? <div><span className='answer'>A:{this.state.answers[0].body}</span> <span>Helpful? {this.state.answers[0].helpfulness} </span> <span>Yes</span> <span>Report</span></div>: ''}
-        {(this.state.answers[1]) ? <div><span className='answer'>A:{this.state.answers[1].body}</span> <span>Helpful? {this.state.answers[1].helpfulness} </span> <span>Yes</span> <span>Report</span></div>: ''}
+      <div id='answerList' className='answers'>
+        {(this.state.answers[0]) ? <div className='answer'><span className='answer-text'>A:{this.state.answers[0].body}</span> <br/> <span>by {this.state.answers[0].answerer_name}</span> <span>Helpful?</span> <u>Yes</u> ({this.state.answers[0].helpfulness}) <u>Report</u> </div>: ''}
+        {(this.state.answers[1]) ? <div id='second-default-answer' className='answer'><span className='answer-text'>A:{this.state.answers[1].body}</span> <br/> <span>by {this.state.answers[1].answerer_name}</span><span>Helpful?</span> <u>Yes</u> ({this.state.answers[1].helpfulness}) <u>Report</u> </div>: ''}
 
-      <div>{(answers.length > 2) ? <span>Load More Answers</span> : ''}</div>
+      <div>{(answers.length > 2) ? <b id='loadMoreAnswersLink' onClick={this.loadMoreAnswers}>Load More Answers</b> : ''}</div>
       </div>
 
     )}
