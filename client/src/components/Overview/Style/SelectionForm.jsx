9c0c76be0694102f35bfa7fabfcff835d771c89e
Ramisa Chowdhury <rrchow97@gmail.com>
2021-04-27 16:05:59 -0400

    Added css to overview, mainly buttons and footer

--- client/src/components/Overview/Style/SelectionForm.jsx
+++ client/src/components/Overview/Style/SelectionForm.jsx
@@ -3,17 +3,21 @@ import QuantityForm from './QuantityForm.jsx'
 
 const SelectionForm = ({ style, handleSize, size, countChange, addToCart }) => (
   <form className="size_quantity_form">
-    <select className="select-size" onChange={handleSize}>
-      <option value="none-selected">- Select Size -</option>
-      {
-        Object.keys(style).map((sku) => {
-          return <option key={sku} value={sku}>{style[sku].size}</option>
-        })
-      }
-    </select>
-    <QuantityForm style={style} size={size} countChange={countChange}/>
+    <div className='select-size-box'>
+      <select className="select-size" onChange={handleSize}>
+        <option value="none-selected">- Select Size -</option>
+        {
+          Object.keys(style).map((sku) => {
+            return <option key={sku} value={sku}>{style[sku].size}</option>
+          })
+        }
+      </select>
+    </div>
+    <div className='quantity-form-box'>
+      <QuantityForm style={style} size={size} countChange={countChange}/>
+    </div>
     <br></br>
-    <button className="add-to-cart" onClick={addToCart}>Add to Cart</button>
+    <button className="add-to-cart" onClick={addToCart}>Add to Cart <span className='add-to-cart-plus'>+</span></button>
   </form>
 )
 
