6a35e2d823bfe9f9c9d93289df2107263dbade63
Ramisa Chowdhury <rrchow97@gmail.com>
2021-04-27 19:48:41 -0400

    Added input placeholders

--- client/src/components/Reviews/ReviewList/Modal/TextDetail.jsx
+++ client/src/components/Reviews/ReviewList/Modal/TextDetail.jsx
@@ -5,6 +5,7 @@ const TextDetail = (props) => (
     <label htmlFor='summary' className='review-add-summary-label'>Review Summary </label>
       <input type='text' className='review-add-summary'
       id='summary' name='summary' maxLength='60'
+      placeholder='Example: Best purchase ever!'
       onChange={props.handleSummary}/>
 
     <br/>
@@ -13,6 +14,7 @@ const TextDetail = (props) => (
     {props.req()}<label htmlFor='body' className='review-add-body-label'>Review body</label>
     <textarea className='review-add-body'
       rows='4' id='body' name='body' maxLength='1000'
+      placeholder='Why did you like the product or not?'
       onChange={props.handleBody}></textarea>
 
     <br/>
