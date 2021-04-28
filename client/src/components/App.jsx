7caea2c4e9ec43e697c660f316eedd750d757049
Ramisa Chowdhury <rrchow97@gmail.com>
2021-04-28 10:52:29 -0400

    deleted commented out code in app.jsx

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,7 +4,6 @@ import { Token } from '../../../config.js';
 import Reviews from './Reviews/Reviews.jsx';
 import Overview from './Overview/Overview.jsx';
 import Questions from './Questions/Questions.jsx';
-// import Relations from './Relations/Relations.jsx';
 import Header from './Header/Header.jsx';
 import { WidgetProvider } from './WidgetContext.jsx'
 
@@ -42,12 +41,6 @@ class App extends React.Component {
       url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products',
       headers: { Authorization: Token },
       success: (data) =>  {
-        // this.setState({ allProducts: data })
-        // this.getProductDetails(data[1].id)
-        // this.getProductStyle(data[1].id)
-        // this.updateProductId(data[1].id)
-        // this.getReviewMeta(data[1].id)
-        // this.getQuestions(data[1].id)
         this.getAllData(data)
       },
       error: (err) => {
@@ -64,14 +57,12 @@ class App extends React.Component {
         headers: { Authorization: Token },
         success: (data) => {
           resolve(data);
-          // this.setState({ product: data })
         },
         error: (err) => {
           reject(err)
         }
       })
     })
-
   }
 
   getProductStyle(id) {
@@ -82,14 +73,12 @@ class App extends React.Component {
         headers: { Authorization: Token },
         success: (data) => {
           resolve(data);
-          // this.setState({ productStyles: data })
         },
         error: (err) => {
           reject(err)
         }
       })
     })
-
   }
 
   updateProductId(productId) {
@@ -120,20 +109,6 @@ class App extends React.Component {
     }).catch(err => {
       console.log(err);
     })
-
-    // $.ajax({
-    //   method: 'GET',
-    //   url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/qa/questions/`,
-    //   headers: {Authorization: Token},
-    //   data: {'product_id': id},
-    //   datatype: 'json'
-    // }).then(data => {
-
-    // }).catch(error => {
-    //   console.log(error);
-    // })
-
-
   }
 
   getQuestions(id) {
@@ -147,15 +122,12 @@ class App extends React.Component {
         datatype: 'json',
         success: (data) => {
           resolve(data)
-          // this.setState({questions: data.results})
         },
         error: (err) => {
           reject(err);
         }
       })
-
     })
-
   }
 
   initializeGetReviewMeta(productId) {
@@ -167,8 +139,6 @@ class App extends React.Component {
         data: { 'product_id': productId },
         success: (data) => {
           resolve(data);
-          // this.setState({ reviewMeta: data })
-          // this.getAverageReview(data.ratings)
         },
         error: (err) => {
           reject(err)
@@ -201,36 +171,24 @@ class App extends React.Component {
     }
 
     return (combinedReviews / totalReviews) || 0
-
-    // this.setState({
-    //   averageReview: (combinedReviews / totalReviews) || 0
-    // });
   }
 
   render() {
     return (
-        <div className='app'>
+      <div className='app'>
 
-          <Header />
+        <Header />
 
-          {/* overview */}
-          <WidgetProvider widget='product overview'>
-            <Overview
-              product={this.state.product}
-              styles={this.state.productStyles}
-              average={this.state.averageReview} />
-          </WidgetProvider>
+        {/* overview */}
+        <WidgetProvider widget='product overview'>
+          <Overview
+            product={this.state.product}
+            styles={this.state.productStyles}
+            average={this.state.averageReview} />
+        </WidgetProvider>
 
-
-          {/* related */}
-          {/* <div className='relations-container' style={{ maxWidth: 1800, marginLeft: 'auto', marginRight: 'auto', marginTop: 64 }}>
-            <Relations
-              product={this.state.product}
-              styles={this.state.productStyles}/>
-          </div> */}
-
-          {/* question */}
-          <Questions productId={this.state.productId} questions={this.state.questions} product={this.state.product}/>
+        {/* question */}
+        <Questions productId={this.state.productId} questions={this.state.questions} product={this.state.product}/>
 
         {/* reviews */}
         <WidgetProvider widget='rating and reviews'>
