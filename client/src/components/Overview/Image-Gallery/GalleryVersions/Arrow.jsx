cfc863bc1ddab3d7d35f3bd802473d0e269d7be3
Joseph McFatridge <josephdanielmcfatridge@gmail.com>
2021-04-29 11:22:31 -0500

    Made some final visual changes to the product overview

--- client/src/components/Overview/Image-Gallery/GalleryVersions/Arrow.jsx
+++ client/src/components/Overview/Image-Gallery/GalleryVersions/Arrow.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 const Arrow = ({direction, handleArrowClick}) => {
   return (
     <div>
-
       <i id={direction}
          className={`fas fa-arrow-${direction} fa-2x`}
          name={direction}></i>
